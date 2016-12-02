039d5590cd7858ea7e66cecd71a2d0930f0262f6
danCarmody <danielcrmdy@gmail.com>
2016-12-01 18:21:01 -0700

    mech team scopes created

---
+++ Scripts/MechTeamController.js
@@ -0,0 +1,372 @@
+angular
+    .module('azLoop')
+    .controller('cntrlTeamController', [$scope, function($scope) {
+        $scope.Thomas = [
+            {
+                src: '',
+                name: 'Thomas Koho',
+                title: 'Team Lead',
+                bio: '',
+            }
+        ],
+
+        $scope.Christian = [
+            {
+                src: '',
+                name: 'Christian Reischauer'
+                title: 'Team Lead',
+                bio: '',
+            }
+        ],
+
+        $scope.Matthew = [
+            {
+                src: '',
+                name: 'Matthew Simpson'
+                title: 'Aero',
+                bio: '',
+            }
+        ],
+
+        $scope.Amy = [
+            {
+                src: '',
+                name: 'Amy Walker'
+                title: 'Safety',
+                bio: '',
+            }
+        ],
+
+        $scope.Omar = [
+            {
+                src: '',
+                name: 'Omar Akesheh'
+                title: 'Consultant',
+                bio: '',
+            }
+        ],
+
+        $scope.Umesh = [
+            {
+                src: '',
+                name: 'Umesh Prasad'
+                title: 'Levitation',
+                bio: '',
+            }
+        ],
+
+        $scope.Avery = [
+            {
+                src: '',
+                name: 'Avery Carter'
+                title: 'Levitation',
+                bio: '',
+            }
+        ],
+
+        $scope.Cody = [
+            {
+                src: '',
+                name: 'Cody Dodd'
+                title: 'Propulsion',
+                bio: '',
+            }
+        ],
+
+        $scope.Alexandra = [
+            {
+                src: '',
+                name: 'Alexandra Indritz'
+                title: 'Propulsion',
+                bio: '',
+            }
+        ],
+
+        $scope.Surya = [
+            {
+                src: '',
+                name: 'Surya Sarvajith'
+                title: 'Propulsion',
+                bio: '',
+            }
+        ],
+
+        $scope.Leann = [
+            {
+                src: '',
+                name: 'Leann Scott'
+                title: 'Propulsion',
+                bio: '',
+            }
+        ],
+
+        $scope.Athena = [
+            {
+                src: '',
+                name: 'Athena Roberts'
+                title: 'Propulsion',
+                bio: '',
+            }
+        ],
+
+        $scope.Padmanabh = [
+            {
+                src: '',
+                name: 'Padmanabh Parekh'
+                title: 'Propulsion',
+                bio: '',
+            }
+        ],
+
+        $scope.Isaiah = [
+            {
+                src: '',
+                name: 'Isaiah Wall'
+                title: 'Propulsion',
+                bio: '',
+            }
+        ],
+
+        $scope.Madison  = [
+            {
+                src: '',
+                name: 'Madison Grayson '
+                title: 'Propulsion',
+                bio: '',
+            }
+        ],
+
+        $scope.Jessica = [
+            {
+                src: '',
+                name: 'Jessica Maschino'
+                title: 'Propulsion',
+                bio: '',
+            }
+        ],
+
+        $scope.Nick  = [
+            {
+                src: '',
+                name: 'Nick Delafuente'
+                title: 'Propulsion',
+                bio: '',
+            }
+        ],
+
+        $scope.KailashRangaraj = [
+            {
+                src: '',
+                name: 'Kailash Rangaraj Bommannan Kuppuraju'
+                title: 'Dynamics',
+                bio: '',
+            }
+        ],
+
+        $scope.RohitKumar = [
+            {
+                src: '',
+                name: 'Rohit Kumar Ladda'
+                title: 'Dynamics',
+                bio: '',
+            }
+        ],
+
+        $scope.Sachin = [
+            {
+                src: '',
+                name: 'Sachin Shivakumar'
+                title: 'Dynamics',
+                bio: '',
+            }
+        ],
+
+        $scope.Vijay = [
+            {
+                src: '',
+                name: 'Vijay Pentapati'
+                title: 'Dynamics',
+                bio: '',
+            }
+        ],
+
+        $scope.Kanniappan = [
+            {
+                src: '',
+                name: 'Kanniappan Krishnarajakumar'
+                title: 'Dynamics',
+                bio: '',
+            }
+        ],
+
+        $scope.Anudeep = [
+            {
+                src: '',
+                name: 'Anudeep Mallarapu'
+                title: 'Modeling lead',
+                bio: '',
+            }
+        ],
+
+        $scope.Mohammed = [
+            {
+                src: '',
+                name: 'Mohammed Rehman'
+                title: 'CAD modeling & Simulation',
+                bio: '',
+            }
+        ],
+
+        $scope.Prerak = [
+            {
+                src: '',
+                name: 'Prerak Dongaonkar'
+                title: 'CAD modeling & Simulation',
+                bio: '',
+            }
+        ],
+
+        $scope.Swapnil = [
+            {
+                src: '',
+                name: 'Swapnil Nimse'
+                title: 'CAD modeling & Simulation',
+                bio: '',
+            }
+        ],
+
+        $scope.Rohan = [
+            {
+                src: '',
+                name: 'Rohan Thakker'
+                title: 'CAD modeling & Simulation',
+                bio: '',
+            }
+        ],
+
+        $scope.Sharan  = [
+            {
+                src: '',
+                name: 'Sharan Kishore'
+                title: 'CAD modeling & Simulation',
+                bio: '',
+            }
+        ],
+
+        $scope.Vijay  = [
+            {
+                src: '',
+                name: 'Vijay Jangid'
+                title: 'CAD modeling & Simulation',
+                bio: '',
+            }
+        ],
+
+        $scope.Qoc  = [
+            {
+                src: '',
+                name: 'Qoc Lam'
+                title: 'Braking Lead',
+                bio: '',
+            }
+        ],
+
+        $scope.James  = [
+            {
+                src: '',
+                name: 'James Casey'
+                title: 'Braking',
+                bio: '',
+            }
+        ],
+
+        $scope.Jonathon  = [
+            {
+                src: '',
+                name: 'Jonathon Barkl'
+                title: 'Braking',
+                bio: '',
+            }
+        ],
+
+         $scope.Alvin  = [
+            {
+                src: '',
+                name: 'Alvin Addisho'
+                title: 'Braking',
+                bio: '',
+            }
+        ],
+
+        $scope.Mohanad  = [
+            {
+                src: '',
+                name: 'Mohanad Al Sabti'
+                title: 'Braking',
+                bio: '',
+            }
+        ],
+
+        $scope.Akshay  = [
+            {
+                src: '',
+                name: 'Akshay Kamath'
+                title: 'Materials & Manufacturing Lead',
+                bio: '',
+            }
+        ],
+
+        $scope.Michelle  = [
+            {
+                src: '',
+                name: 'Michelle Moreno'
+                title: 'Materials & Manufacturing',
+                bio: '',
+            }
+        ],
+
+        $scope.Karthik  = [
+            {
+                src: '',
+                name: 'Karthik Sankaran'
+                title: 'Materials & Manufacturing',
+                bio: '',
+            }
+        ],
+
+        $scope.Gautham  = [
+            {
+                src: '',
+                name: 'Gautham Ayyapessur'
+                title: 'Materials & Manufacturing',
+                bio: '',
+            }
+        ],
+
+        $scope.Rohith  = [
+            {
+                src: '',
+                name: 'Rohith Mathews'
+                title: 'Materials & Manufacturing',
+                bio: '',
+            }
+        ],
+
+        $scope.Carl  = [
+            {
+                src: '',
+                name: 'Carl Allred'
+                title: 'Materials & Manufacturing',
+                bio: '',
+            }
+        ],
+
+        $scope.Rahul  = [
+            {
+                src: '',
+                name: 'Rahul Verma'
+                title: 'Materials & Manufacturing',
+                bio: '',
+            }
+        ],
+    }]);
