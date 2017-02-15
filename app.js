9e63e9ce1a46425e08cf633f41827808e7091560
Zach Alden <jzalden96@gmail.com>
2017-02-14 21:46:24 -0700

    reorganize file structure

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 angular.module('azLoop', ['ui.bootstrap']);
 
-$('.navbar-sticky').affix({offset:{top:function(){return this.top=$('.navbar-sticky').offset().top }}});
+$('.navbar-sticky').affix({
+    offset: {
+        top: function() {
+            return this.top = $('.navbar-sticky').offset().top
+        }
+    }
+});
